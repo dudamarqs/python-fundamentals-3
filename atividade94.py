8cfd15db5f295138b1a89f60d02fcaa58a1cd12b
dudamarqs <eduardamarquesnoleto@gmail.com>
2024-08-02 19:54:33 -0300

    Registration system - Joining lists and dictionaries

--- atividade94.py
+++ atividade94.py
@@ -5,28 +5,37 @@ pessoas = list()
 while True:
     pessoa.clear()
     pessoa['nome'] = str(input('Nome: '))
+    
     while True:
-        pessoa['sexo'] = str(input('Sexo: [M/F] '))
-        if pessoa['sexo'] in 'MmFf':
+        pessoa['sexo'] = str(input('Sexo: [M/F] ')).upper()[0]
+        if pessoa['sexo'] in 'MF':
             break
         print('Por favor, digite M ou F para indicar seu sexo.')
+    
     pessoa['idade'] = int(input('Idade: '))
     soma += pessoa['idade']
     pessoas.append(pessoa.copy())
+    
     while True:
-        pergunta = input('Deseja continuar? [S/N] ')
-        if pergunta in 'SsNn':
+        pergunta = input('Deseja continuar? [S/N] ').upper()[0]
+        if pergunta in 'SN':
             break
         print('Digite apenas S ou N para indicar sua resposta.')
-    if pergunta in 'Nn':
+    if pergunta == 'N':
         break
 
 print()
-print(f'{len(pessoas)} foram cadastradas.')
+print(f'A) {len(pessoas)} pessoas foram cadastradas.')
 media = soma / len(pessoas)
-print(f'A média de idade entre os cadastrados é {media} anos.')
-print('As mulheres cadastradas foram: ', end='')
+print(f'B) A média de idade entre os cadastrados é {media:5.2f} anos.')
+print('C) As mulheres cadastradas foram: ', end='')
 
 for p in pessoas:
-    if pessoa['sexo'] in 'Ff':
-        print(f'{p['nome']} ', end='')
+    if p['sexo'] in 'Ff':
+        print(f'{p["nome"]} ', end='')
+
+print()
+print(f'D) Pessoas com idade acima da média: ', end='')
+for p in pessoas:
+    if p['idade'] > media:
+        print(f'{p["nome"]} ', end='')
